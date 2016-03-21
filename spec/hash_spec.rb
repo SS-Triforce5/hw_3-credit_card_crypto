a3830b19cb9ac5d5143404244a4f5ebda59ff380
pengyuchen <pengyu@nlplab.cc>
2016-03-21 13:14:49 +0800

    finish hash spec

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -9,29 +9,34 @@ card_details = [
 ]
 
 cards = card_details.map { |c| CreditCard.new(c[:num], c[:exp], c[:name], c[:net]) }
-
+puts cards[0]
 describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
     describe 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      cards.each { |card| card.to_s.hash.must_equal card.to_s.hash }
     end
 
     describe 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      cards.combination(2).each { |card_a, card_b| card_a.to_s.hash.wont_equal card_b.to_s.hash }
     end
   end
 
   describe 'Test cryptographic hashing' do
     describe 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      cards.each { |card| card.to_s.hash_secure.must_equal card.to_s.hash_secure }
     end
 
     describe 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      cards.combination(2) { |card_a, card_b| card_a.to_s.hash.wont_equal card_b.to_s.hash }
     end
 
     describe 'Check regular hash not same as cryptographic hash' do
       # TODO: Check that each card's hash is different from its hash_secure
+      cards.each { |card| card.to_s.hash.wont_equal card.to_s.hash_secure }
     end
   end
 end
